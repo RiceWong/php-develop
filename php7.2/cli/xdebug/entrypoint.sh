8f80abe3b44ded204d4c3c01e62798f2099b8c4d
ricky wong <rickywonng@gmail.com>
2019-06-10 11:32:11 +0800

    xdebug runtime image

--- php7.2/cli/xdebug/entrypoint.sh
+++ php7.2/cli/xdebug/entrypoint.sh
@@ -1,3 +1,4 @@
-#! /bin/sh
+#! /bin/bash
 export PYTHONPATH=\${PYTHONPATH}:/usr/local/dbgp/pythonlib
-nohup /usr/local/dbgp/pydbgpproxy -i 0.0.0.0:10001 -d 127.0.0.1:10000  > /var/log/dbgp.log 2>&1
+nohup /usr/local/dbgp/pydbgpproxy -i 0.0.0.0:10001 -d 127.0.0.1:10000  > /var/log/dbgp.log 2>&1 &
+exec "$@"
